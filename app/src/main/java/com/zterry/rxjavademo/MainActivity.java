bcaf4798a04c84ac2fc28501c81198de47b0191d
liuzhanta <liuzhanta@huoban.com>
2017-06-08 17:50:35 +0800

    Initial commit

---
+++ app/src/main/java/com/zterry/rxjavademo/MainActivity.java
@@ -0,0 +1,136 @@
+package com.zterry.rxjavademo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import io.reactivex.Flowable;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                range();
+
+                observer();
+            }
+        });
+
+    }
+
+    private static final String TAG = "MainActivity";
+    private void observer() {
+
+        Observable mObservable= Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                Log.d(TAG, "subscribe: ");
+                e.onNext(1);
+                e.onNext(2);
+                e.onComplete();
+            }
+        });
+
+        Observer mObserver=new Observer<Integer>() {
+            //这是新加入的方法，在订阅后发送数据之前，
+            //回首先调用这个方法，而Disposable可用于取消订阅
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "onSubscribe() called with: d = [" + d + "]");
+            }
+
+            @Override
+            public void onNext(Integer value) {
+                Log.d(TAG, "onNext() called with: value = [" + value + "]");
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "onError() called with: e = [" + e + "]");
+
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete() called");
+            }
+        };
+
+        mObservable.subscribe(mObserver);
+    }
+
+    private void range() {
+        Flowable.range(0,10)
+                .subscribe(new Subscriber<Integer>() {
+                    Subscription sub;
+                    //当订阅后，会首先调用这个方法，其实就相当于onStart()，
+                    //传入的Subscription s参数可以用于请求数据或者取消订阅
+                    @Override
+                    public void onSubscribe(Subscription s) {
+                        Log.w("TAG","onsubscribe start");
+                        sub=s;
+                        sub.request(1);
+                        Log.w("TAG","onsubscribe end");
+                    }
+
+                    @Override
+                    public void onNext(Integer o) {
+                        Log.w("TAG","onNext--->"+o);
+                        sub.request(1);
+                    }
+                    @Override
+                    public void onError(Throwable t) {
+                        t.printStackTrace();
+                    }
+                    @Override
+                    public void onComplete() {
+                        Log.w("TAG","onComplete");
+                    }
+                });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
